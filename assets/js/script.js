8d1740310acd545073696bcaddcdc8401b28edb5
Albada <NamelessOAO@gmail.com>
2019-04-09 17:10:35 +0800

    before postcss
    before postcss

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+import bird from '../images/bird.jpg';
+import mouse from '../images/squirrel.jpg';
+
+$('#img-bird').attr("src",bird);
+$('#img-mouse').attr("src",mouse);
+
+$(document).ready(function () {
+	// when a hanburger menu is clicked
+	let $ul = $(".nav__list"),
+		 $menu = $(".nav__menu");
+
+	$($menu).click(function () {
+		// toggle menu-click Class
+		$ul.toggleClass("menu-click");
+	}); // end click event handler
+});
